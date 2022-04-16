019ec09a87ea12bf5fa588dd557e1e6fe8e40955
sidharthr8 <sidharth.r@nutanix.com>
2022-04-16 10:31:28 +0530

    add doc to rdmdeploy.py

--- pythontools/rdm/bin/rdmdeploy.py
+++ pythontools/rdm/bin/rdmdeploy.py
@@ -2,12 +2,32 @@ import click
 from pythontools.rdm import rdmdeploy
 
 @click.command()
-@click.option('--name')
-@click.option('--nodepool', type=click.Choice(['private', 'global']), default='private')
-@click.option('--nocmsp', is_flag=True)
+@click.option('--name', help='unique name for the deployment, if not supplied, the '
+                             '\nname will be calculated using the logged in user\'s name'
+                             '\n appended with a random floating point number')
+@click.option('--nodepool', type=click.Choice(['private', 'global']), default='private',
+              help='the nodepool on which to deploy. by default deployed on prism-real-node'
+                   '\n private node pool.')
+@click.option('--nocmsp', is_flag=True, help='if this flag is provided, then the'
+                                             '\ndeployed pc will not have cmsp')
 # @click.option('--pcbuildurl')
-@click.option('--duration', type=int, default=24)
+@click.option('--duration', type=int, default=24, help='duration of the deployment.\n'
+                                                       'maximum duration is subject to restrictions imposed by admins and rdm policies')
 def main(name, nodepool, nocmsp, duration):
+    """
+    \b
+    Deploy a latest master PE/PC using rdm on either global/private nodepools and with/without
+    cmsp, and register the PE to the PC.
+    The pc build will be one which is smoke passed. In case smoke passed builds are
+    not available, the deployment will be aborted.
+    The deployment status can be tracked using rdmstatus.py script
+    \b
+    Examples:
+    1. On the prism-real-node private nodepool (which is by default), deploy a PE-PC with cmsp for a
+    duration of 4 days: rdmdeploy.py --duration 96
+    2. On the global nodepool, deploy a PE-PC without cmsp, for the default
+    duration of 1 day: rdmdeploy.py --nodepool global --nocmsp
+    """
     res = rdmdeploy.main(name, nodepool, nocmsp, duration)
 
     if res.status_code == 200:
