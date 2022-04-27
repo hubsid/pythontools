0d2e3c01a2a59b8c0e62e2a0adc9ba8ffce3f9f4
sidharthr8 <sidharth.r@nutanix.com>
2022-04-27 12:19:30 +0530

    include categories

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 def getscripts():
     scripts = []
     code_path = 'pythontools'
-    for path in ['common', 'rdm', 'iam', 'personal']:
+    for path in ['common', 'rdm', 'iam', 'personal', 'categories']:
         path = os.path.join(code_path, path)
         if os.path.isdir(path):
             binpath = os.path.join(path, 'bin')
